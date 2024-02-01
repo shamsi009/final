5684e75e1b94fbf60c0d37001f4b6c4fd3f22601
abdullah <l1s21bsse0075@ucp.edu.pk>
2024-02-01 11:58:24 +0500

    adding code

---
+++ E-learning.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class E-learning{
+private:
+int id;
+string course;
+public:
+E-learning();
+public void set_id(int id)();
+public void get_id()();
+public void set_course(string course)();
+public void string get_course()();
+
+
+};
+int main()
+{
+	
+	return 0;
+}
